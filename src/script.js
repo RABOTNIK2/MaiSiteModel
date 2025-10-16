41dd2cd7c94cd34d579d737b209863f1dc25364a
RABOTNIK2 <boyarchuk.2007@mail.ru>
2025-10-16 18:51:34 +0300

    first

---
+++ src/script.js
@@ -0,0 +1,83 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import init from './init';
+
+import './style.css';
+
+const { sizes, camera, scene, canvas, controls, renderer } = init();
+
+camera.position.set(0, 2, 5)
+
+const floor = new THREE.Mesh(
+	new THREE.PlaneGeometry(10, 10),
+	new THREE.MeshStandardMaterial({
+		color: '#444444',
+		metalness: 0,
+		roughness: 0.5
+	})
+);
+
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+hemiLight.position.set(0, 5, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+dirLight.position.set(-8, 12, 8);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+scene.add(dirLight);
+
+const loader = new GLTFLoader();
+loader.load(
+	'/models/University/University.gltf',
+	(gltf) => {
+		console.log("success");
+		console.log(gltf);
+		gltf.scene.scale.set(5, 5, 5);
+		scene.add(gltf.scene);
+	},
+	(progress) => {
+		console.log("progress");
+		console.log(progress);
+	},
+	(error) => {
+		console.log("error");
+		console.log(error);
+	}
+
+);
+
+const tick = () => {
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(tick);
+};
+tick();
+
+
+window.addEventListener('resize', () => {
+
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	renderer.render(scene, camera);
+});
+
+window.addEventListener('dblclick', () => {
+	if (!document.fullscreenElement) {
+		canvas.requestFullscreen();
+	} else {
+		document.exitFullscreen();
+	}
+});
